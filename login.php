75cd7845bfcb62a0e07a3a282d50e5c8ae9d3651
solomon-wamutu <solomonwamutu16@gmail.com>
2024-02-12 18:39:51 +0300

    login

--- login.php
+++ login.php
@@ -11,20 +11,10 @@ if (isset($_SESSION['SESS_NAME']) != "") {
 echo $nam; ?>
 <?php global $error;
 echo $error; ?>
-<!-- <br>
+<br>
 <center><font size="4" >
-<form action="login_action.php" method="post" id="myform" >
-Username : 
-<input type="text" name="username" value="" > 
-<br>
-<br>
-Password : 
-<input type="password" name="password" value="" >
-<br>
-<br>
-<input type="submit" name="login" value="login" > 
-</form></font>
-</center> -->
+
+</center>
 
 <!DOCTYPE html>
 <html>
